a94c8200f1a60b4472ed997614b62714f1aeb72d
viprs <viprs@qq.com>
2017-02-19 16:55:42 +0800

    将sendmail集成到Main函数里

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from lib.Logger import Logger
 from lib.HTMLTestRunner import HTMLTestRunner
 from testcase.admin_login_logout.admin_login_correction import AdminLoginCorrection
 from testcase.admin_login_logout.admin_login_failure import AdminLoginFail
+from lib.sendmail import send_email
+
+FROM_ADDR = u"zelin_test@163.com"
+FROM_PSWD = u"Zelin123456"  # 163设置的第三方授权码
+TO_ADDR = u"zelin_test@163.com"
+
 
 def suite():
     suite = unittest.TestSuite()
@@ -20,12 +26,12 @@ def suite():
 if __name__ == "__main__":
     logger = Logger().getlog()
     logger.info('start testcase...')
-
-    fp = open('./result/test_result_%s.html' % time.strftime("%Y-%m-%d %H-%M-%S"), 'wb')
+    report_path = './result/test_result_%s.html' % time.strftime("%Y-%m-%d %H-%M-%S")
+    fp = open(report_path, 'wb')
     runner = HTMLTestRunner(stream=fp,
                             title=u'测试报告生成范例',
                             description=u"测试用例执行情况：")
     runner.run(suite())
     fp.close()
-
+    send_email(FROM_ADDR, FROM_PSWD, TO_ADDR, u"冒烟测试报告", report_path)
     logger.info('stop testcase...')
